ecc2b42d2fbb78025eca45a340798a2bb873666b
NotaCSstudent <mahmudbabu94@gmail.com>
2021-09-14 13:43:47 -0400

    Add files via upload

---
+++ rustvscpp/main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+
+
+
+
+
+int main()
+{
+    vector<int> res;
+    res.push_back(100);
+    res.push_back(200);
+    res.push_back(300);
+    res.push_back(400);
+    res.push_back(500);
+}
--- rustvscpp/main.rs
+++ rustvscpp/main.rs
@@ -1,1 +1,17 @@
-fnmain()
+
+
+fn main()
+{
+    let mut res : Vec<i32> = Vec::new();
+    res.push(100);
+    res.push(200);
+    res.push(300);
+    res.push(400);
+    res.push(500);
+    for i in res
+    {
+        println!("{}",i);
+    }
+    
+
+}
