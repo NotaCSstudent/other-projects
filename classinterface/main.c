bec5bed2904bd03cf7a54a24159ce3444ece3a7a
NotaCSstudent <mahmudbabu94@gmail.com>
2021-09-15 18:00:37 -0400

    Create main.c

---
+++ classinterface/main.c
@@ -0,0 +1,21 @@
+//C does not have classes sadly
+
+
+#include <stdio.h>
+
+struct X
+{
+    int x;
+    int y;
+    int z;
+    
+    
+};
+
+
+int main()
+{
+    struct X;
+    
+    printf("Hello");
+}
